7c0b659b6b5ce08542feb9c1863e3991e96882b8
AnubhavUjjawal <anubhav.u16@iiits.in>
2019-09-28 17:39:39 +0530

    final updates

--- main.py
+++ main.py
@@ -25,23 +25,49 @@ fullfillment_res = {
                   "textToSpeech": "this is a simple response"
                 }
               }
+            ],
+            "suggestions": [
+              {
+                "title": "List all your Services"
+              },
+              {
+                "title": "Subscription Plans"
+              },
+              {
+                "title": "My Shipped Orders"
+              },
+              {
+                "title": "Shipping Service"
+              }
             ]
           }
         },
       }
     }
 
+plans_res = {
+        "intent": "actions.intent.OPTION",
+        "data": {
+          "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+          "listSelect": {
+            "title": "Plans Available",
+            "items": []
+          }
+        }
+      }
+
 @app.route('/dialogflow', methods=['POST'])
 def  handle_dialoglow():
   app.logger.warning(pprint.pformat(request.json))
   x = request.json['queryResult']['intent']['displayName']
   res_string = ""
+  res = copy.deepcopy(fullfillment_res)
 
   if x == 'Order':
     o_no = int(request.json['queryResult']['parameters']['OrderNumber'])
     shipping_order = client.test['shipping'].find_one({'id': o_no})
     # print(shipping_order)
-    res_string = f'Your Order with id {shipping_order["id"]} shipped to {shipping_order["to_street"]}, {shipping_order["to_city"]}, {shipping_order["to_state"]} scheduled to be delivered on {shipping_order["delivery_date"].strftime("%d %B %Y")}'
+    res_string = f'Your Order with shipment ID {shipping_order["id"]}, shipped to {shipping_order["to_street"]}, {shipping_order["to_city"]}, {shipping_order["to_state"]} is scheduled to be delivered on {shipping_order["delivery_date"].strftime("%d %B %Y")}'
   elif x == 'ListServices':
     services = client.test['services'].find()
     res_string = f'The services offered by us are: '
@@ -56,8 +82,26 @@ def  handle_dialoglow():
   elif x == 'TrackingService':
     tracking = client.test['services'].find_one({"id": "tracking"})
     res_string = f'An introduction to our Tracking service: {tracking["description"]}'
+  elif x == 'Plans':
+    plans = client.test['plans'].find()
+    res_string = f'Please find the details of our Plans.'
+    plans_json = copy.deepcopy(plans_res)
+    for plan in plans:
+      item = {"title": plan["name"], "description": plan["description"], "optionInfo": {"key": str(plan["id"]),"synonyms": []}}
+      plans_json["data"]["listSelect"]["items"].append(item)
+    res['payload']['google']['systemIntent'] = plans_json
+  elif x == 'Shipping':
+    shippings = client.test['shipping'].find()
+    res_string = f'Here are your shipped orders and their status: '
+    plans_json = copy.deepcopy(plans_res)
+    plans_json["data"]["listSelect"]["title"] = "Your Shipped Orders"
+    for ships in shippings:
+      item = {"title": f'Order ID: {ships["id"]}', "description": f'Status: {ships["status"]}', "optionInfo": {"key": str(ships["id"]),"synonyms": []}}
+      plans_json["data"]["listSelect"]["items"].append(item)
+    res['payload']['google']['systemIntent'] = plans_json
+  else:
+    res_string = f'Sorry, I couldn\'t understand you. Please try again'
 
-  res = copy.deepcopy(fullfillment_res)
   res['payload']['google']['richResponse']['items'][0]['simpleResponse']['textToSpeech'] = res_string
   res['fulfillmentText'] = res_string
   return res
